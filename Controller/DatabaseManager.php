40507912e4b8748d841b8760f0f73c27b74d4e83
jordydp <jdepril@outlook.com>
2022-03-21 10:53:06 +0100

    Connect to DataBase usinge DatabaseManager

---
+++ Controller/DatabaseManager.php
@@ -0,0 +1,39 @@
+<?php
+
+// This class will manage the connection to the database
+// It will be passed on the other classes who need it
+class DatabaseManager
+{
+    // These are private: only this class needs them
+    private string $host;
+    private string $user;
+    private string $password;
+    private string $dbname;
+    // This one is public, so we can use it outside of this class
+    // We could also use a private variable and a getter (but let's not make things too complicated at this point)
+    public PDO $connection;
+
+    public function __construct(string $host, string $user, string $password, string $dbname)
+    {
+        //Set any user and password information
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+        $this->dbname = $dbname;
+    }
+
+    public function connect(): void
+    {
+        try {
+            $dsn = "mysql:host={$this->host};port=8889;dbname={$this->dbname}";
+            // TODO: make the connection to the database
+            $this->connection = new PDO($dsn, $this->user, $this->password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "connected successfully";
+        }
+        catch(PDOException $e)
+        {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+}
--- index.php
+++ index.php
@@ -6,15 +6,25 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
+//include config
+require_once 'config.php';
+
 //include all your model files here
 require 'Model/Article.php';
 //include all your controllers here
+require 'Controller/DatabaseManager.php';
 require 'Controller/HomepageController.php';
 require 'Controller/ArticleController.php';
+
+// connect to database
+$databaseManager = new DatabaseManager($config['host'], $config['user'], $config['password'], $config['dbname']);
+$databaseManager->connect();
 
 // Get the current page to load
 // If nothing is specified, it will remain empty (home should be loaded)
 $page = $_GET['page'] ?? null;
+
+
 
 // Load the controller
 // It will *control* the rest of the work to load the page
